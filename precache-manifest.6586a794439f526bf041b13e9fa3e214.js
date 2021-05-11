bbf0992b51c90452d07d62c0255245eb5d5c26e0
JinalBhanushali0628 <jinalbhanushali0628@gmail.com>
2021-05-11 16:42:38 -0400

    Updates

---
+++ precache-manifest.6586a794439f526bf041b13e9fa3e214.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb805fe63cd9f98e08e6763691bb5cea",
+    "url": "/JinalBhanushali/ADIT-IEI.git/index.html"
+  },
+  {
+    "revision": "bb389e4b09b2e75b241b",
+    "url": "/JinalBhanushali/ADIT-IEI.git/static/css/main.19bf4666.chunk.css"
+  },
+  {
+    "revision": "16ddec1629ca9dce7093",
+    "url": "/JinalBhanushali/ADIT-IEI.git/static/js/2.401f563a.chunk.js"
+  },
+  {
+    "revision": "dd207babaeb913476be46155cc504ef5",
+    "url": "/JinalBhanushali/ADIT-IEI.git/static/js/2.401f563a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb389e4b09b2e75b241b",
+    "url": "/JinalBhanushali/ADIT-IEI.git/static/js/main.05725f40.chunk.js"
+  },
+  {
+    "revision": "2ceee8c70d54fa2e5466",
+    "url": "/JinalBhanushali/ADIT-IEI.git/static/js/runtime-main.6a3f8347.js"
+  },
+  {
+    "revision": "b10969c6615897d6ac80df7439475c06",
+    "url": "/JinalBhanushali/ADIT-IEI.git/static/media/event2.b10969c6.jpg"
+  },
+  {
+    "revision": "70f976fa046a4ca9ae0cddebd4edac21",
+    "url": "/JinalBhanushali/ADIT-IEI.git/static/media/happy.70f976fa.gif"
+  },
+  {
+    "revision": "ce672444c010099c4eac4ef248874cb9",
+    "url": "/JinalBhanushali/ADIT-IEI.git/static/media/iei_logo.ce672444.png"
+  },
+  {
+    "revision": "e0ea88c5db6ffde45479a44baa87da20",
+    "url": "/JinalBhanushali/ADIT-IEI.git/static/media/join.e0ea88c5.gif"
+  }
+]);
